0fa4b970274109060b53a6f8bb8c31af8e9ae299
Durga Rohith <rohithssj23@gmail.com>
2026-07-03 23:31:49 +0530

    Time: 3 ms (60.54%), Space: 52.4 MB (65.46%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -5,11 +5,10 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 1) {
                 count++;
-                maxCount = Math.max(maxCount, count);
             } else {
-                maxCount = Math.max(maxCount, count);
                 count = 0;
             }
+               maxCount = Math.max(maxCount, count);
         }
         return maxCount;
     }
