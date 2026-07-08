facbc7819f2c249053914bfb5e7652a2e83367f9
Durga Rohith <rohithssj23@gmail.com>
2026-07-08 23:55:52 +0530

    Time: 1 ms (99.85%), Space: 46.7 MB (65.16%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+    ArrayList<String> arr = new ArrayList<>();
+    for(int i = 1;i<=n;i++){
+        if(i%3==0&&i%5==0){
+            arr.add("FizzBuzz");
+        }
+        else if(i%3==0){
+            arr.add("Fizz");
+        }
+
+        else if(i%5==0){
+            arr.add("Buzz");
+        }
+        else{
+            String temp = String.valueOf(i);
+            arr.add(temp);
+        }
+    }
+    return arr;
+    }
+}
