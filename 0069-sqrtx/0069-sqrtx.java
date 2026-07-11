45cdce7fed08d0a4c9636cdf3c83d744e0d6c2a2
Durga Rohith <rohithssj23@gmail.com>
2026-07-11 23:55:23 +0530

    Time: 1 ms (97.64%), Space: 42.5 MB (61.35%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,7 +1,7 @@
 class Solution {
     public int mySqrt(int x) {
-        double pow = Math.floor(Math.pow(x,0.5));
-        int ans = (int) pow;
-        return ans;
+        int pow = (int)Math.floor(Math.pow(x,0.5));
+        
+        return pow;
     }
 }
