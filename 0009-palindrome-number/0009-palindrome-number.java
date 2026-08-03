39997d6daf1034aabac3ee32dc8768c3e4e408dd
Durga Rohith <rohithssj23@gmail.com>
2026-08-03 15:59:21 +0530

    Time: 5 ms (80.94%), Space: 45.9 MB (53.79%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,18 +1,17 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        int org = x;
-        int rev = 0;
-        if(x<0) return false;
-        while(org>0){
-            int digit = org%10;
-            rev = (rev*10)+digit;
-            org = org/10;
-        }
-        if(rev==x){
-            return true;
-        }
-        else{
-            return false;
-    }
+       if(x<0) return false;
+       int rev = 0;
+       int copy = x;
+       while(copy>0){
+        int digit = copy%10;
+        rev = (rev*10)+digit;
+        copy=copy/10;
+       }
+       if(rev==x){
+        return true;
+       }
+        return false;
+       
     }
 }
